557e06bdb1405f9bd7b154e119398f97c4078cf7
Shu <zhangshu_happy@hotmail.com>
2024-01-08 08:57:41 +1300

    download CV added

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
     let icon = document.querySelector(".hamburger-icon");  
     icon.classList.toggle("open");
   }
+
+function downloadCV() {
+  var cvFilePath = 'CV.pdf';
+
+  var downloadLink = document.createElement('a');
+  downloadLink.href = cvFilePath;
+
+  // Set the download attribute with a desired filename
+  downloadLink.download = 'ShuZhang_CV.pdf';
+
+  // Append the anchor to the body
+  document.body.appendChild(downloadLink);
+
+  // Trigger a click on the anchor element
+  downloadLink.click();
+
+  // Remove the anchor from the body
+  document.body.removeChild(downloadLink);
+}
+
+// Attach the download function to the button click event
+document.getElementById('downloadButton').addEventListener('click', downloadCV);
